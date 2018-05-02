a6dc4adb3aa154ea19139dda1399893dbfc5588b
fareekerubo <kerubofarida@gmail.com>
2018-05-02 16:47:29 +0300

    Second Commit

--- iris-flowers.py
+++ iris-flowers.py
@@ -2,6 +2,10 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from scipy.spatial import distance
+
+def euc(a,b):
+    return distance.euclidean(a,b)
 
 class ScrappyKNN():
     def fit(self, features_train,labels_train):
@@ -10,12 +14,24 @@ class ScrappyKNN():
 
 
     def predict(self,features_test):
-        prediction = []
+        predictions = []
         for item in features_test:
+            label = self.closest(item)
             #determine which other point is closest and use that to set label value
             predictions.append(label)
 
         return predictions
+
+    def closest(self, item):
+        best_dist = euc(item, self.features_train[0])
+        best_index =0
+        for i in range(1, len(self.features_train)):
+            dist = euc(item, self.features_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return self.labels_train[best_index]
+
 
 iris = datasets.load_iris()
 
@@ -24,8 +40,8 @@ labels = iris.target
 
 features_train,features_test,labels_train,labels_test = train_test_split(features,labels,test_size=.5)
 
-
-my_classifier = KNeighborsClassifier()
+my_classifier = ScrappyKNN()
+#my_classifier = KNeighborsClassifier()
 my_classifier.fit(features_train, labels_train)
 
 prediction = my_classifier.predict(features_test)
